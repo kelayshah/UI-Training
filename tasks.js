acbbe0176e4370f52f999690486db2e972f179c1
Kelay <shah.kelay10@gmail.com>
2018-01-31 17:25:15 +0530

    added more functionalities -- localstorage left

--- tasks.js
+++ tasks.js
@@ -1,56 +1,472 @@
-var callerID;
-var taskID = 22;
-
-var monthNames = ["January", "February", "March", "April", "May", "June",
+let monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
-function removeTask(taskid){
 
-	if (confirm("Are you sure you want to delete the task?")) {
-		var t = document.getElementById(taskid);
-		var toDelete = t.parentNode.parentNode.parentNode;
-		var parentDiv = toDelete.parentNode;
-		parentDiv.removeChild(toDelete);
-	}
-}
+/*
+-----------------------------------------------------------------------------------
+Data (Model)
+-----------------------------------------------------------------------------------
+*/
 
-function openPopup(userid){
-	callerID = userid;
-	//console.log(callerID);
-	var form = document.getElementById("add-task-form");
-	form.reset();
-	window.location.href = "#add-task-popup";
-}
+let model = {
+		tasks: {
+				task1: {
+					task_id: "task1",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user1"
+				 },
+				 task2: {
+					task_id: "task2",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "High",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user1"
+				 },
+				 task3: {
+					task_id: "task3",
+					task_name: "Some task name is very very very long",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user1"
+				 },
+				 task4: {
+					task_id: "task4",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "High",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user2"
+				 },
+				 task5: {
+					task_id: "task5",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user2"
+				 },
+				 task6: {
+					task_id: "task6",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user2"
+				 },
+				 task7: {
+					task_id: "task7",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "High",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user3"
+				 },
+				 task8: {
+					task_id: "task8",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user3"
+				 },
+				 task9: {
+					task_id: "task9",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user3"
+				 },
+				 task10: {
+					task_id: "task10",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "Normal",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user3"
+				 },
+				 task11: {
+					task_id: "task11",
+					task_name: "Some task name",
+					deadline: "2018-01-31",
+					status: "todo",
+					priority: "High",
+					task_description: "This task is related to XYZ. The important thing is that this task is ABC and so DEF.",
+					assignee_id: "user3"
+				 }
+			},
 
-function addTask(){
+		users : {
+			 user1: {
+				user_id : "user1",
+				name: "User 1",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 },
+			 user2: {
+			 	user_id : "user2",
+				name: "User 2",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 },
+			 user3: {
+			 	user_id : "user3",
+				name: "User 3",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 },
+			 user4: {
+			 	user_id : "user4",
+				name: "User 4",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 },
+			 user5: {
+			 	user_id : "user5",
+				name: "User 5",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 },
+			 user6: {
+			 	user_id : "user6",
+				name: "User 6",
+				role: "Product Engineer",
+				email: "abc@def.com",
+				skype: "abc",
+				image_src: "user.jpeg"
+			 }
+		},
 
-	var newTask = document.createElement("li");
-	newTask.setAttribute("class","item-task");
+		selectedUserId: "user1",
+		taskNum: 12
+		};
 
-	var d = new Date(document.getElementById("new-task-deadline").value);
-	var dateString = monthNames[d.getMonth()]+" "+d.getDate()+" , "+d.getFullYear();
 
-	var taskName = document.getElementById("new-task-name").value;
-	if(taskName.length>22){
-		taskName = taskName.substring(0,20)+"..";
-	}
+/*
+-----------------------------------------------------------------------------------
+Controller (Connects Model and View)
+-----------------------------------------------------------------------------------
+*/
 
-	newTask.innerHTML = '<div class="task '+ document.getElementById("new-task-status").value +' pri-'+document.getElementById("new-task-priority").value+'">\
-	 					<section class="task-name">' + taskName + '</section>\
-	 					<ul class="task-details">\
-	 						<li class="edit-del-task"><button class="edit-task-btn">Edit</button><button id="t'+ taskID +'" onclick="removeTask(this.id);return false;" class="del-task-btn">Delete</button></li>\
-	 						<li>Deadline: '+ dateString + '</li>\
-	 						<li>Priority: '+ document.getElementById("new-task-priority").value +'</li>\
-	 					</ul>\
-	 				</div>';
+let controller = {
 
-	 taskID++;
+		init : function(){
+			userTasks.init();
 
-	var elem = document.getElementById(callerID).parentNode;
-	
-	elem.childNodes[7].appendChild(newTask);
-	document.location.href = "#";
+			document.getElementById("head-add-task-btn").addEventListener('click',function(event){
+				userTasks.showAddTaskForm(false);
+				window.location.href = "#add-task-popup";
+			},false);
 
-}
+			document.body.addEventListener('click',function(event){
+				let target = event.target;
+				
+				if(target.matches("#view-task-edit")){
+					let taskId = target.closest("#view-task-popup").dataset.taskId;
+					userTasks.showEditTaskForm(taskId);
+					window.location.href = "#edit-task-popup";
+				}
 
+				else if(target.matches("#view-task-delete")){
+					let taskId = target.closest("#view-task-popup").dataset.taskId;
+					if (confirm("Are you sure you want to delete the user?")){
+						controller.removeTask(taskId);
+						document.location.href = "#";
+					}
+				}
+
+				else if(target.matches(".add-task-btn")){
+					let userId = target.closest(".col-1-4").id;
+					model.selectedUserId = userId;
+					userTasks.showAddTaskForm(true);
+					window.location.href = "#add-task-popup";
+				}
+
+				if(!target.closest(".item-task"))
+					return;
+
+				let taskId = target.closest(".item-task").id;
+
+				if(target.matches(".edit-task-btn")){
+					userTasks.showEditTaskForm(taskId);
+					window.location.href = "#edit-task-popup";
+				}
+
+				else if(target.matches(".del-task-btn")){
+					if (confirm("Are you sure you want to delete the user?")){
+						controller.removeTask(taskId);
+					}
+				}
+
+				else if(target.closest(".task")){
+					userTasks.showTaskDetails(taskId);
+					window.location.href = "#view-task-popup";
+				}
+
+			},false);
+
+			document.body.addEventListener('submit',function(event){
+				let target = event.target;
+				event.preventDefault();
+
+				if(target.matches("#add-task-form")){
+					controller.addNewTask();
+				}
+
+				if(target.matches("#edit-task-form")){
+					let taskId = target.closest("#edit-task-popup").dataset.taskId;
+					controller.editTask(taskId);
+				}
+
+			},false);
+		},
+
+		dragStartHandler: function(event){
+			event.dataTransfer.setData("text/plain", event.target.id);
+			event.dataTransfer.dropEffect = "move";
+		},
+
+		dragOverHandler: function(event){
+			event.preventDefault();
+			event.dataTransfer.dropEffect = "move";
+		},
+
+		dropHandler: function(event){
+			event.preventDefault();
+			let movedTaskId = event.dataTransfer.getData("text");
+			let newAssignee = event.target.closest(".col-1-4").id;
+			model.tasks[movedTaskId].assignee_id = newAssignee;
+			event.target.closest(".task-list").appendChild(document.getElementById(movedTaskId));
+		},
+
+		getTaskList: function(){
+			return model.tasks;
+		},
+
+		getSelectedUser: function(){
+			return model.selectedUserId;
+		},
+
+		getUserList: function(){
+			return model.users;
+		},
+
+		addNewTask : function(){
+			let newTask = {
+				task_id: "task"+model.taskNum++, 
+				task_name: document.getElementById("new-task-name").value,
+				deadline: document.getElementById("new-task-deadline").value,
+				status: document.getElementById("new-task-status").value,
+				priority: document.getElementById("new-task-priority").value,
+				task_description: document.getElementById("new-task-description").value,
+				assignee_id: document.getElementById("new-task-assignee").value,
+			};
+
+			model.tasks[newTask.task_id] = newTask;
+			userTasks.addTaskToView(newTask);
+		},
+
+		removeTask: function(taskToDeleteId){
+			delete model.tasks[taskToDeleteId];
+			userTasks.removeTaskFromView(taskToDeleteId);
+		},
+
+		editTask: function(taskToEditId){
+			let taskToEdit = model.tasks[taskToEditId];
+			
+			let oldStatus = taskToEdit.status;
+			let oldPriority = taskToEdit.priority;
+			let oldAssignee = taskToEdit.assignee_id;
+
+			taskToEdit.task_name = document.getElementById("edit-task-name").value;
+			taskToEdit.deadline = document.getElementById("edit-task-deadline").value;
+			taskToEdit.status = document.getElementById("edit-task-status").value;
+			taskToEdit.priority = document.getElementById("edit-task-priority").value;
+			taskToEdit.task_description = document.getElementById("edit-task-description").value;
+			taskToEdit.assignee_id = document.getElementById("edit-task-assignee").value;
+			
+			userTasks.displayEditedTask(taskToEdit,oldAssignee,oldStatus,oldPriority);
+		}
+};
+
+
+/*
+-----------------------------------------------------------------------------------
+View
+-----------------------------------------------------------------------------------
+*/
+
+let userTasks = {
+
+		init : function(){
+			
+			let tasks_list = controller.getTaskList();
+
+			for(task in tasks_list){
+				let newTask = tasks_list[task];
+				this.addTaskToView(newTask);
+			}
+		},
+
+		getDateString : function(dateNumber){
+			let date = new Date(dateNumber);
+			let dateString = monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+			return dateString;
+		},
+
+		addTaskToView : function(taskToAdd){
+			
+			let newTask = document.createElement("li");
+			newTask.setAttribute("class","item-task");
+			newTask.setAttribute("id",taskToAdd.task_id);
+			newTask.setAttribute("draggable","true");
+
+			newTask.innerHTML = '<div class="task '+ taskToAdd.status +' pri-'+ taskToAdd.priority +'">\
+			 					<section class="task-name">' + taskToAdd.task_name + '</section>\
+			 					<ul class="task-details">\
+			 						<li class="edit-del-task"><button class="edit-task-btn">Edit</button>\
+			 						<button class="del-task-btn">Delete</button></li>\
+			 						<li id="task-deadline">Deadline: '+ userTasks.getDateString(taskToAdd.deadline) + '</li>\
+			 						<li id="task-priority">Priority: '+ taskToAdd.priority +'</li>\
+			 					</ul>\
+			 				</div>';
+
+			let assignee = document.getElementById(taskToAdd.assignee_id);
+			let taskList = assignee.querySelector(".task-list");
+			taskList.appendChild(newTask);
+			document.location.href = "#";
+		},
+
+		removeTaskFromView : function(taskToDeleteId){
+			let taskToDelete = document.getElementById(taskToDeleteId);
+			let taskList = taskToDelete.closest(".task-list");
+			taskList.removeChild(taskToDelete);
+		},
+
+		showTaskDetails : function(taskId){
+			let all_tasks = controller.getTaskList();
+			let all_users = controller.getUserList();
+			let taskToShow = all_tasks[taskId];
+
+			document.getElementById("view-task-popup").dataset.taskId = taskId;
+
+			document.getElementById("view-task-name").innerHTML = "<strong>Name:</strong> "+taskToShow.task_name;
+			document.getElementById("view-task-description").innerHTML = "<strong>Description:</strong> "+taskToShow.task_description;
+			document.getElementById("view-task-status").innerHTML = "<strong>Status:</strong> "+taskToShow.status;
+			document.getElementById("view-task-priority").innerHTML = "<strong>Priority:</strong> "+taskToShow.priority;
+			document.getElementById("view-task-deadline").innerHTML = "<strong>Deadline:</strong> "+userTasks.getDateString(taskToShow.deadline);
+			document.getElementById("view-task-assignee").innerHTML = "<strong>Assignee:</strong> "+all_users[taskToShow.assignee_id].name;
+		},
+
+		showAddTaskForm : function(userKnown){
+			var form = document.getElementById("add-task-form");
+			form.reset();
+
+			let all_users = controller.getUserList();
+
+			let userList = document.getElementById("new-task-assignee");
+			while (userList.firstChild) {
+    			userList.removeChild(userList.firstChild);
+			}
+
+			if(userKnown){
+				let userItem = document.createElement("option");
+				userItem.innerHTML = all_users[controller.getSelectedUser()].name;
+				userItem.value = controller.getSelectedUser();
+				userList.appendChild(userItem);
+				return;
+			}
+
+			for(user in all_users){
+				let userItem = document.createElement("option");
+				userItem.innerHTML = all_users[user].name;
+				userItem.value = all_users[user].user_id;
+				userList.appendChild(userItem);
+			}
+		},
+
+		showEditTaskForm : function(taskId){
+			var form = document.getElementById("edit-task-form");
+			form.reset();
+
+			let all_tasks = controller.getTaskList();
+			let all_users = controller.getUserList();
+			let taskToEdit = all_tasks[taskId];
+
+			let userList = document.getElementById("edit-task-assignee");
+			while (userList.firstChild) {
+    			userList.removeChild(userList.firstChild);
+			}
+			for(user in all_users){
+				let userItem = document.createElement("option");
+				userItem.innerHTML = all_users[user].name;
+				userItem.value = all_users[user].user_id;
+				if(all_users[user].user_id == taskToEdit.assignee_id){
+					userItem.selected = "selected";
+				}
+				userList.appendChild(userItem);
+			}
+
+			document.getElementById("edit-task-popup").dataset.taskId = taskId;
+
+			document.getElementById("edit-task-name").value = taskToEdit.task_name;
+			document.getElementById("edit-task-description").value = taskToEdit.task_description;
+			document.getElementById("edit-task-status").value = taskToEdit.status;
+			document.getElementById("edit-task-priority").value = taskToEdit.priority;
+			document.getElementById("edit-task-deadline").value = taskToEdit.deadline;
+		},
+
+		displayEditedTask : function(editedTask,oldAssignee,oldStatus,oldPriority){
+			let task = document.getElementById(editedTask.task_id);
+
+			task.querySelector(".task-name").innerHTML = editedTask.task_name;
+			task.querySelector("#task-deadline").innerHTML = "Deadlne: "+userTasks.getDateString(editedTask.deadline);
+			task.querySelector("#task-priority").innerHTML = "Priority: "+editedTask.priority;
+
+			if(oldPriority!= editedTask.priority){
+				task.querySelector(".task").classList.add("pri-"+editedTask.priority);
+				task.querySelector(".task").classList.remove("pri-"+oldPriority);
+			}
+
+			if(oldStatus!= editedTask.status){
+				task.querySelector(".task").classList.add(editedTask.status);
+				task.querySelector(".task").classList.remove(oldStatus);
+			}
+
+			if(oldAssignee!=editedTask.assignee_id){
+				let assignee = document.getElementById(editedTask.assignee_id);
+				let taskList = assignee.querySelector(".task-list");
+				taskList.appendChild(task);
+			}
+			document.location.href = "#";
+		}
+};
+
+
+controller.init();
--- users.js
+++ users.js
@@ -1,43 +1,228 @@
-var userID = 22;
+/*
+-----------------------------------------------------------------------------------
+Data (Model)
+-----------------------------------------------------------------------------------
+*/
 
-function removeUser(userid){
-	if (confirm("Are you sure you want to delete the user?")) {
-		var u = document.getElementById(userid);
-		var toDelete = u.parentNode.parentNode.parentNode;
-		var parentDiv = toDelete.parentNode;
-		parentDiv.removeChild(toDelete);
-	}
-}
+let model = {
+			users : {
+					 user1: {
+						user_id : "user1",
+						name: "User 1",
+						role: "Product Engineer",
+						email: "abc@def.com",
+						skype: "abc",
+						image_src: "user.jpeg"
+					 },
+					 user2: {
+					 	user_id : "user2",
+						name: "User 2dwgbsdvGvfefabdvrebadsvws",
+						role: "Product EngineerrwdbSDVCHRBBfsvxc",
+						email: "abc@def.comwrhSBFDVCXGWRsfdvcx",
+						skype: "abcrgsVDXCGRSDVCRGEsfvc",
+						image_src: "user.jpeg"
+					 },
+					 user3: {
+					 	user_id : "user3",
+						name: "User 3",
+						role: "Product Engineer",
+						email: "abc@def.com",
+						skype: "abc",
+						image_src: "user.jpeg"
+					 },
+					 user4: {
+					 	user_id : "user4",
+						name: "User 4",
+						role: "Product Engineer",
+						email: "abc@def.com",
+						skype: "abc",
+						image_src: "user.jpeg"
+					 },
+					 user5: {
+					 	user_id : "user5",
+						name: "User 5",
+						role: "Product Engineer",
+						email: "abc@def.com",
+						skype: "abc",
+						image_src: "user.jpeg"
+					 },
+					 user6: {
+					 	user_id : "user6",
+						name: "User 6",
+						role: "Product Engineer",
+						email: "abc@def.com",
+						skype: "abc",
+						image_src: "user.jpeg"
+					 }
+					},
+
+			userNum : 7
+} 
+
+/*
+-----------------------------------------------------------------------------------
+Controller (Connects Model and View)
+-----------------------------------------------------------------------------------
+*/
+
+let controller = {
+
+		init : function(){
+			userInfo.init();
+
+			document.getElementById("add-user-btn").addEventListener('click',function(event){
+				let form = document.getElementById("add-user-form");
+				form.reset();
+				window.location.href = "#add-user-popup";
+			},false);
+
+			document.body.addEventListener('click',function(event){
+				let target = event.target;
+				
+				if(!target.closest(".user"))
+					return;
+
+				let userId = target.closest(".user").id;
+
+				if(target.matches(".edit-user-btn")){
+					userInfo.showEditUserForm(userId);
+					window.location.href = "#edit-user-popup";
+				}
+
+				else if(target.matches(".del-user-btn")){
+					if (confirm("Are you sure you want to delete the user?")){
+						controller.removeUser(userId);
+					}
+				}
+			},false);
+
+			document.body.addEventListener('submit',function(event){
+				let target = event.target;
+				event.preventDefault();
+
+				if(target.matches("#add-user-form")){
+					controller.addNewUser();
+				}
+
+				if(target.matches("#edit-user-form")){
+					let userId = target.closest("#edit-user-popup").dataset.userId;
+					controller.editUser(userId);
+				}
+
+			},false);
+		},
+
+		getUserList: function(){
+			return model.users;
+		},
+
+		addNewUser : function(){
+			let newUser = {
+				user_id: "user"+model.userNum++, 
+				name: document.getElementById("new-user-name").value,
+				skype: document.getElementById("new-user-skype").value,
+				email: document.getElementById("new-user-email").value,
+				role: document.getElementById("new-user-role").value,
+				image_src: document.getElementById("new-user-image").value
+			};
+
+			model.users[newUser.user_id] = newUser;
+			userInfo.addUserToView(newUser);
+		},
+
+		removeUser: function(userToDeleteId){
+			delete model.users[userToDeleteId];
+			userInfo.removeUserFromView(userToDeleteId);
+		},
+
+		editUser: function(userToEditId){
+			let userToEdit = model.users[userToEditId];
+
+			userToEdit.name = document.getElementById("edit-user-name").value;
+			userToEdit.skype = document.getElementById("edit-user-skype").value;
+			userToEdit.email = document.getElementById("edit-user-email").value;
+			userToEdit.role = document.getElementById("edit-user-role").value;
+			userToEdit.image_src = document.getElementById("edit-user-image").value;
+			
+			userInfo.displayEditedUser(userToEdit);
+		}
+};
 
 
-function openPopup(){
-	var form = document.getElementById("add-user-form");
-	form.reset();
-	window.location.href = "#add-user-popup";
-}
+/*
+-----------------------------------------------------------------------------------
+View
+-----------------------------------------------------------------------------------
+*/
 
-function addUser(){
+let userInfo = {
 
-	var newUser = document.createElement("li");
-	newUser.setAttribute("class","user");
+		init : function(){
+			
+			let users_list = controller.getUserList();
 
-	newUser.innerHTML = '<section class="user-section">\
-	 				<section class="user-name">'+document.getElementById("new-user-name").value+'</section>\
-	 				<section class="edit-del-user"><button class="edit-user-btn">Edit</button><button id="u'+ userID +'" onclick="removeUser(this.id);return false;"\
-	 				 class="del-user-btn">Delete</button></section>\
-	 				<section class="role">Role: '+document.getElementById("new-user-role").value+'</section>\
-	 				<section class="skypeID">Skype ID: '+document.getElementById("new-user-skype").value+'</section>\
-	 				<section class="email">Email ID: '+document.getElementById("new-user-email").value+'</section>\
-	 			</section>\
-	 			<section class="user-image">\
-	 				<img src="'+document.getElementById("new-user-image").value.substring(12,) +'" alt="User '+userID+'">\
-	 			</section>';
-	userID++;
+			for(user in users_list){
+				let newUser = users_list[user];
+				this.addUserToView(newUser);
+			}
+		},
 
-	var elem = document.getElementById("all-users");
+		addUserToView : function(userToAdd){
+			
+			let newUser = document.createElement("li");
+			newUser.setAttribute("class","user");
+			newUser.setAttribute("id",userToAdd.user_id);
+
+			newUser.innerHTML = '<section class="user-section">\
+				 				<section title="'+userToAdd.name+'"class="user-name">'+userToAdd.name+'</section>\
+				 				<section class="edit-del-user"><button class="edit-user-btn">Edit</button><button\
+				 				 class="del-user-btn">Delete</button></section>\
+				 				<section title="'+userToAdd.role+'"class="role">Role: '+userToAdd.role+'</section>\
+				 				<section title="'+userToAdd.skype+'"class="skypeID">Skype ID: '+userToAdd.skype+'</section>\
+				 				<section title="'+userToAdd.email+'"class="email">Email ID: '+userToAdd.email+'</section>\
+					 			</section>\
+					 			<section class="user-image">\
+					 				<img src="'+userToAdd.image_src +'" alt=" '+userToAdd.name+'">\
+					 			</section>';
+
+			var elem = document.getElementById("all-users");
 	
-	elem.appendChild(newUser);
-	window.location.href = "#";
+			elem.appendChild(newUser);
+			window.location.href = "#";
+		},
 
-}
+		removeUserFromView : function(userToDeleteId){
+			let userToDelete = document.getElementById(userToDeleteId);
+			let userList = document.getElementById("all-users");
+			userList.removeChild(userToDelete);
+		},
 
+		showEditUserForm : function(userId){
+			var form = document.getElementById("edit-user-form");
+			form.reset();
+
+			let all_users = controller.getUserList();
+			let userToEdit = all_users[userId];
+
+			document.getElementById("edit-user-popup").dataset.userId = userId;
+
+			document.getElementById("edit-user-name").value = userToEdit.name;
+			document.getElementById("edit-user-skype").value = userToEdit.skype;
+			document.getElementById("edit-user-email").value = userToEdit.email;
+			document.getElementById("edit-user-role").value = userToEdit.role;
+			//document.getElementById("edit-user-image").value = userToEdit.image_src;
+		},
+
+		displayEditedUser : function(editedUser){
+			let user = document.getElementById(editedUser.user_id);
+
+			user.querySelector(".user-name").innerHTML = editedUser.name;
+			user.querySelector(".role").innerHTML = "Role: "+editedUser.role;
+			user.querySelector(".skypeID").innerHTML = "Skype ID: "+editedUser.skype;
+			user.querySelector(".email").innerHTML = "Email ID: "+editedUser.email;
+			document.location.href = "#";
+		}
+};
+
+
+controller.init();
